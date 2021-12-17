f5b6334e68d4ad0646204f5b3f54e1b0aaa165dc
goutamir007 <92869545+goutamir007@users.noreply.github.com>
2021-12-17 23:04:52 +0530

    Update AAD user.ps1

--- AAD user.ps1
+++ AAD user.ps1
@@ -1,13 +1,14 @@
 #powershell script to create new user in aad 
 
-Step 1: Open Azure Portal and open azure cloud shell
-Step 2: Select Powershell and type the following commands to set the cloud shell for the use.
+#Step 1: Open Azure Portal and open azure cloud shell
+#Step 2: Select Powershell and type the following commands to set the cloud shell for the use.
        Install-Module Az
        Connect-AzAccount -UseDeviceAuthentication
        Select-AzSubscription -Subscription "xxxxxxxname"
-Step 3: To connect to the Azure Active Directory we need provide the following commands:
+#Step 3: To connect to the Azure Active Directory we need provide the following commands:
         Connect-AzureAD      
-Step 4: We need to provide a password for the user .
+#Step 4: We need to provide a password to the user.
+
         $PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
         $PasswordProfile.Password = "*********"
         $params = @{
@@ -17,5 +18,8 @@ Step 4: We need to provide a password for the user .
         UserPrincipalName = "xxxxxxxxx.onmicrosoft.com" 
         MailNickName = "xxxx"
         }
+OR
 
-
+#STEP 4: $PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
+         $PasswordProfile.Password = "XXXXXX"
+         New-AzureADUser -DisplayName "YYYYYY" -PasswordProfile $PasswordProfile -UserPrincipalName "*****@contoso.com" -AccountEnabled $true -MailNickName "ABCDEFG"
