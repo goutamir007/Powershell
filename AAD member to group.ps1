c20eaeecd40544260f9955d3f3f404d16c818120
goutamir007 <92869545+goutamir007@users.noreply.github.com>
2021-12-18 14:36:57 +0530

    Add files via upload

---
+++ AAD member to group.ps1
@@ -0,0 +1,11 @@
+#Add a member to a group
+
+Step 1: Open Azure Portal and open Azure cloud shell
+Step 2: Select Powershell and type the following commands to set the cloud shell to use.
+        Install-Module Az
+        Connect-AzAccount -UseDeviceAuthentication
+        Select-AzSubscription -Subscription "xxxxxxxname"
+Step 3: To connect to the Azure Active Directory we need provide the following commands:
+        Connect-AzureAD
+Step 4: Add-AzureADGroupMember -ObjectId "<ObjectId>" -RefObjectId "<ReferenceId>"
+        
