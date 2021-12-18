dc27b3f2b6163db1792b98be13991786767da16a
goutamir007 <92869545+goutamir007@users.noreply.github.com>
2021-12-18 14:16:17 +0530

    Add files via upload

---
+++ Roleassignment.ps1
@@ -0,0 +1,10 @@
+#TO ASSIGN A ROLE TO THE USER FOR A RESOURCE AT SUBSCRIPTION LEVEL
+
+Step 1: Open Azure Portal and open azure cloud shell
+Step 2: Select Powershell and type the following commands to set the cloud shell for the use.
+       Install-Module Az
+       Connect-AzAccount -UseDeviceAuthentication
+       Select-AzSubscription -Subscription "xxxxxxxname"
+Step 4: New-AzRoleAssignment -ObjectId "xxxxxxx"
+        -RoleDefinitionName "<rolename>"
+        -Scope "/subscriptions/subscriptionid/resourcegroups/resourcegroupname/providers/Microsoft.Storage/storageAccounts/<storageaccountname>"
