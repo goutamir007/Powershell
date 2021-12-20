9ffa920eda83537bb69718c3adb9f12254467e19
goutamir007 <92869545+goutamir007@users.noreply.github.com>
2021-12-20 14:27:11 +0530

    Update AAD member to group.ps1

--- AAD member to group.ps1
+++ AAD member to group.ps1
@@ -7,5 +7,7 @@ Step 2: Select Powershell and type the following commands to set the cloud shell
         Select-AzSubscription -Subscription "xxxxxxxname"
 Step 3: To connect to the Azure Active Directory we need provide the following commands:
         Connect-AzureAD
+        ![Link1](https://github.com/goutamir007/Powershell/blob/main/Images/PicsArt_12-20-01.37.38.jpg)
+        
 Step 4: Add-AzureADGroupMember -ObjectId "<ObjectId>" -RefObjectId "<ReferenceId>"
 
