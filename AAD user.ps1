e73ade18ad7d2040830eee591bdc0f55c853b2fb
goutamir007 <92869545+goutamir007@users.noreply.github.com>
2021-12-20 13:08:40 +0530

    Update AAD user.ps1

--- AAD user.ps1
+++ AAD user.ps1
@@ -1,4 +1,4 @@
-#powershell script to create new user in aad 
+#To create new user in AAD 
 
 #Step 1: Open Azure Portal and open azure cloud shell
 #Step 2: Select Powershell and type the following commands to set the cloud shell for the use.
