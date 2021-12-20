52a23e375b147b54f2c240e58ce32677242b7e1d
goutamir007 <92869545+goutamir007@users.noreply.github.com>
2021-12-20 14:04:16 +0530

    Update AAD member to group.ps1

--- AAD member to group.ps1
+++ AAD member to group.ps1
@@ -1,4 +1,4 @@
-**Add a member to a group**
+**Add a member to a group** **Sign in**
 
 Step 1: Open Azure Portal and open Azure cloud shell
 Step 2: Select Powershell and type the following commands to set the cloud shell to use.
