c03328da6dfb00d5064fa363872b7f6201945e8d
goutamir007 <92869545+goutamir007@users.noreply.github.com>
2021-12-20 14:04:52 +0530

    Update AAD member to group.ps1

--- AAD member to group.ps1
+++ AAD member to group.ps1
@@ -1,4 +1,4 @@
-** Add a member to a group ** 
+#Add a member to a group
 
 Step 1: Open Azure Portal and open Azure cloud shell
 Step 2: Select Powershell and type the following commands to set the cloud shell to use.
@@ -9,4 +9,3 @@ Step 3: To connect to the Azure Active Directory we need provide the following c
         Connect-AzureAD
 Step 4: Add-AzureADGroupMember -ObjectId "<ObjectId>" -RefObjectId "<ReferenceId>"
 
-![](../images.md/PicsArt_12-20-01.37.38.jpg)
